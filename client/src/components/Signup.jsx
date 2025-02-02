176b5c8cd42c6bbd9a5c92549b89ad85ede098b4
Francissundarraj <francissundarraj02@gmail.com>
2025-02-03 03:11:39 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import banner from "../Assets/lm2.png"
+import banner from "../Assets/bannner.png"
 import { useNavigate } from "react-router-dom"
 import { createUserWithEmailAndPassword } from "firebase/auth"
 import { fetchSignInMethodsForEmail } from "firebase/auth"
